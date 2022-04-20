98c3b7746f06575464f04d4cc08fba34b72c52ce
JadKHaddad <jadkhaddad@gmail.com>
2022-04-20 23:45:12 +0200

    WEBSOCKET Skel.

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,18 +1,27 @@
+use futures_util::SinkExt;
+use futures_util::StreamExt;
 use parking_lot::RwLock;
 use poem::{
-    handler,
+    get, handler,
     listener::TcpListener,
     middleware::AddData,
     post,
-    web::{Data, Html, Multipart},
-    EndpointExt, Route, Server,
+    web::{
+        websocket::{Message, WebSocket},
+        Data, Html, Multipart,
+    },
+    EndpointExt, IntoResponse, Route, Server,
 };
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::{
     collections::HashMap,
     process::Child,
     str,
-    sync::{atomic::AtomicBool, Arc},
+    sync::{atomic::AtomicBool, atomic::Ordering, Arc},
+    time::Duration,
 };
+use tokio::sync::watch::Sender;
+use tokio::time::sleep;
 mod lib;
 
 #[handler]
@@ -47,6 +56,83 @@ async fn upload(
     }
 }
 
+#[handler]
+pub async fn ws(
+    ws: WebSocket,
+    clients: Data<&Arc<RwLock<HashMap<String, Sender<String>>>>>,
+    information_thread_running: Data<&Arc<AtomicBool>>,
+) -> impl IntoResponse {
+    let clients = Arc::clone(&clients);
+    let tokio_information_thread_clients = Arc::clone(&clients);
+    let information_thread_running = Arc::clone(&information_thread_running);
+    ws.on_upgrade(move |socket| async move {
+        let (tx, mut rx) = tokio::sync::watch::channel(String::from("channel"));
+        let (mut sink, mut stream) = socket.split();
+        let id = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_micros()
+            .to_string();
+        let id_rx = id.clone();
+        let id_tx = id.clone();
+        {
+            let mut clients_guard = clients.write();
+            clients_guard.insert(id, tx);
+        }
+        tokio::spawn(async move {
+            while let Some(Ok(msg)) = stream.next().await {
+                if let Message::Text(rec) = msg {
+                    println!("WEBSOCKET: Received message: [{}], [{}]", rec, id_rx);
+                }
+            }
+            let mut clients_guard = clients.write();
+            clients_guard.remove(&id_rx);
+            println!("WEBSOCKET: SENDER DISCONNECTED: [{}]", id_rx);
+        });
+
+        tokio::spawn(async move {
+            while rx.changed().await.is_ok() {
+                let msg = String::from(&*rx.borrow());
+                if sink.send(Message::Text(msg)).await.is_err() {
+                    break;
+                }
+            }
+            println!("WEBSOCKET: RECEIVER DISCONNECTED: [{}]", id_tx);
+        });
+
+        //run information thread
+        if !information_thread_running.load(Ordering::SeqCst) {
+            let tokio_information_thread_running = information_thread_running.clone();
+            tokio::spawn(async move {
+                loop {
+                    {
+                        let guard = tokio_information_thread_clients.read();
+                        if guard.len() < 1 {
+                            tokio_information_thread_running.store(false, Ordering::SeqCst);
+                            println!("INFORMATION THREAD: Terminating!");
+                            break;
+                        }
+                        println!("INFORMATION THREAD: Running!");
+                        for (id, tx) in guard.iter() {
+                            match tx.send(String::from("INFORMATION THREAD")) {
+                                Ok(_) => {}
+                                Err(e) => {
+                                    println!(
+                                        "INFORMATION THREAD: ERROR SENDING MESSAGE TO [{}]:\n{:?}",
+                                        id, e
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    sleep(Duration::from_secs(3)).await;
+                }
+            });
+            information_thread_running.store(true, Ordering::SeqCst);
+        }
+    })
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     //create projects dir
@@ -60,6 +146,10 @@ async fn main() -> Result<(), std::io::Error> {
         Arc::new(RwLock::new(HashMap::new()));
     let currently_installing_projects = Arc::new(AtomicBool::new(false));
 
+    //clients
+    let clients: Arc<RwLock<HashMap<String, Sender<String>>>> =
+        Arc::new(RwLock::new(HashMap::new()));
+    let information_thread_running = Arc::new(AtomicBool::new(false));
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "poem=debug");
     }
@@ -67,8 +157,11 @@ async fn main() -> Result<(), std::io::Error> {
 
     let app = Route::new()
         .at("/upload", post(upload))
+        .at("/ws", get(ws))
         .with(AddData::new(installing_tasks))
-        .with(AddData::new(currently_installing_projects));
+        .with(AddData::new(currently_installing_projects))
+        .with(AddData::new(clients))
+        .with(AddData::new(information_thread_running));
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .run(app)
         .await
