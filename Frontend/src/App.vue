9e64d433deac8d07031f500053899e3f8f903740
JadKHaddad <jadkhaddad@gmail.com>
2022-05-01 20:04:42 +0200

    Frontend: format

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h2>connected clients: {{ connected_clients_count }} | running_tests_count:
-    {{ running_tests_count }}
+    <h2>
+      connected clients: {{ connected_clients_count }} | running_tests_count:
+      {{ running_tests_count }}
     </h2>
     <router-view />
   </div>
@@ -20,16 +21,13 @@ export default {
   methods: {
     connenctWebsocket() {
       this.ws = new WebSocket(`ws://${location.host}/api/master/ws`);
-      this.ws.onopen = () => {
-      };
-      this.ws.onclose = () => {
-        
-      };
+      this.ws.onopen = () => {};
+      this.ws.onclose = () => {};
       this.ws.onmessage = (event) => {
         console.log(event.data);
         const data = JSON.parse(event.data);
         const event_type = data.event_type;
-        if (event_type === "INFORMATION"){
+        if (event_type === "INFORMATION") {
           this.connected_clients_count = data.event.connected_clients_count;
           this.running_tests_count = data.event.running_tests_count;
         }
@@ -38,7 +36,7 @@ export default {
   },
   created() {
     this.connenctWebsocket();
-  }
+  },
 };
 </script>
 
