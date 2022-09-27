de56e6bac72b66490cd5659eee838a011a424902
jha <jha@gselectronic.com>
2022-09-27 12:27:32 +0200

    design updates

--- Frontend/src/views/Preview.vue
+++ Frontend/src/views/Preview.vue
@@ -1,35 +1,37 @@
 <template>
   <div>
-    <div class="check-content text">{{ content }}</div>
+    <h3 class="text">Preview: {{ pid }} | Script: {{ id }}</h3>
+    <pre class="code">
+      <code id="python-code" class="cm-s-default" ref="code-block"></code>
+    </pre>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Check",
+  name: "Preview",
   props: ["pid", "id"],
-  data() {
-    return {
-      content: null,
-    };
-  },
-  created() {
-    this.loading = true;
+  mounted() {
     fetch(`/api/master/preview_script/${this.pid}/${this.id}`, {
       method: "POST",
     })
       .then((data) => data.json())
       .then((data) => {
         if (data.success) {
-          this.content = data.content;
+          CodeMirror.runMode(
+            "\n" + data.content,
+            'python',
+            this.$refs["code-block"]
+          );
         } else {
-          
+
         }
       })
       .catch();
-  },
+  }
 };
 </script>
 
 <style>
+
 </style>
