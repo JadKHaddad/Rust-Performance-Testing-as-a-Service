8e4190cddf865118970598d23265143351e3f6c7
JadKHaddad <jadkhaddad@gmail.com>
2022-09-29 21:48:26 +0200

    Control Skel. Todo: WS impl.

--- Frontend/src/views/Control.vue
+++ Frontend/src/views/Control.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     Control
-    <Test v-for="test in reversedTests" :key="test.id" :test="test" :darkTheme="darkTheme" @stop_me="stop(test.id)" @delete_me="del(test.id)"
-      @restart_me="restart(test.info)" @download_me="download(test.id)" />
+    <Test v-for="test in reversedTests" :key="test.id" :test="test" :darkTheme="darkTheme" @stop_me="stop(test.info)" @delete_me="del(test.info)"
+      @restart_me="restart(test.info)" @download_me="download(test.info)" />
   </div>
 </template>
 
@@ -20,8 +20,86 @@ export default {
       tests: [],
     };
   },
-  methods: {
-    connenctWebsocket() {}
+  methods: { // TODO methods are so similar to Script.vue. Maybe we can merge them?
+    connenctWebsocket() {
+      //TODO
+    },
+    restart(test_info) {
+      fetch(`/api/worker/start_test/${test_info.project_id}/${test_info.script_id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(test_info),
+      })
+        .then((data) => data.json())
+        .then((data) => {
+          if (data.success) {
+            // ws will add the new test
+            // let test = data.content;
+            // this.tests.push(test);
+            // this.ws.send(
+            //   JSON.stringify({
+            //     event_type: "TEST_STARTED",
+            //     event: {
+            //       test: test,
+            //     },
+            //   })
+            // );
+          } else {
+            console.log(data.error);
+            //notify
+            this.$root.notification(
+              `Error running test: ${data.error}`,
+              "danger",
+              0
+            );
+          }
+        })
+        .catch(() => { });
+    },
+    stop(test_info) {
+      fetch(`/api/master/stop_test/${test_info.project_id}/${test_info.script_id}/${test_info.id}`, {
+        method: "POST",
+      })
+        .then((data) => data.json())
+        .then((data) => {
+          if (data.success) {
+            let test = this.tests.find((test) => test.id === test_id);
+            if (test) {
+              test.status = 1;
+            }
+          } else {
+            console.log(data.error);
+          }
+        })
+        .catch(() => {});
+    },
+    del(test_info) {
+      fetch(`/api/master/delete_test/${test_info.project_id}/${test_info.script_id}/${test_info.id}`, {
+        method: "POST",
+      })
+        .then((data) => data.json())
+        .then((data) => {
+          if (data.success) {
+            this.tests = this.tests.filter((test) => test.id !== test_info.id);
+          } else {
+            console.log(data.error);
+          }
+        })
+        .catch();
+    },
+    download(test_info) {
+      fetch(`/api/master/download_test/${test_info.project_id}/${test_info.script_id}/${test_info.id}`, {
+        method: "GET",
+      })
+        .then((response) => response.blob())
+        .then((blob) => {
+          var objectUrl = URL.createObjectURL(blob);
+          window.location.href = objectUrl;
+        })
+        .catch(() => {});
+    },
   },
   computed: {
     reversedTests() {
