e52426bd5ad25016618ec3ba9e8b90acdffe6007
jha <jha@gselectronic.com>
2022-10-05 11:23:43 +0200

    Ter. Dock. Upd.

--- Terraform/Docker/main.tf
+++ Terraform/Docker/main.tf
@@ -16,11 +16,27 @@ provider "docker" {
 locals {
   root_path_tmp       = "/${replace(abspath(path.root), ":", "")}"
   root_path           = replace(local.root_path_tmp, "////", "/")
-  context_path        = "../../"
-  dockerfiles_path    = "Dockerfiles"
-  data_path           = "${local.root_path}/../../Dockerfiles/Performance-Testing-Data"
-  container_data_path = "/home/app/Backend/Performance-Testing-Data"
+  paths = {
+    context_path        = "../../"
+    dockerfiles_path    = "Dockerfiles"
+    data_path           = "${local.root_path}/../../Dockerfiles/Performance-Testing-Data"
+    container_data_path = "/home/app/Backend/Performance-Testing-Data"
+  }
+  ports = {
+    master_internal_port       = 3000
+    master_external_port       = 3000
+    worker_internal_port       = 5000
+    frontend_internal_port     = 80
+    frontend_external_port     = 7000
+    loadbalancer_internal_port = 80
+    loadbalancer_external_port = 5000
+    entrypoint_internal_port   = 80
+    entrypoint_external_port   = 8000
+  }
   docker_registry     = "localhost:32000"
+
+
+  container_restart_policy = "unless-stopped"
 }
 
 resource "docker_network" "network" {
@@ -38,15 +54,15 @@ resource "docker_container" "redis" {
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
 
 resource "docker_image" "builder" {
   name         = "builder:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.builder"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.builder"
   }
 }
 
@@ -54,8 +70,8 @@ resource "docker_image" "runner" {
   name         = "runner:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.runner"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.runner"
   }
 }
 
@@ -63,8 +79,8 @@ resource "docker_image" "master" {
   name         = "${local.docker_registry}/master-release:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.master-release"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.master-release"
   }
 }
 
@@ -73,81 +89,81 @@ resource "docker_container" "master" {
   image = docker_image.master.image_id
   name  = "master"
   ports {
-    internal = 3000
-    external = 3000
+    internal = local.ports.master_internal_port
+    external = local.ports.master_external_port
   }
   volumes {
-    container_path = local.container_data_path
-    host_path      = local.data_path
+    container_path = local.paths.container_data_path
+    host_path      = local.paths.data_path
   }
   env = ["REDIS_HOST=${docker_container.redis.name}"]
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
 
 resource "docker_image" "worker" {
   name         = "${local.docker_registry}/worker-release:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.worker-release"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.worker-release"
   }
 }
 
-resource "docker_container" "worker-1" {
+resource "docker_container" "worker_1" {
   user  = "root"
   image = docker_image.worker.image_id
   name  = "worker-1"
   ports {
-    internal = 5000
+    internal = local.ports.worker_internal_port
     external = 5001
   }
   volumes {
-    container_path = local.container_data_path
-    host_path      = local.data_path
+    container_path = local.paths.container_data_path
+    host_path      = local.paths.data_path
   }
   env = [
     "REDIS_HOST=${docker_container.redis.name}",
     "MASTER_IP=${docker_container.master.name}:${docker_container.master.ports[0].internal}",
-    "WORKER_NAME=worker-1:5000"
+    "WORKER_NAME=worker-1:${local.ports.worker_internal_port}"
   ]
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
 
-resource "docker_container" "worker-2" {
+resource "docker_container" "worker_2" {
   user  = "root"
   image = docker_image.worker.image_id
   name  = "worker-2"
   ports {
-    internal = 5000
+    internal = local.ports.worker_internal_port
     external = 5002
   }
   volumes {
-    container_path = local.container_data_path
-    host_path      = local.data_path
+    container_path = local.paths.container_data_path
+    host_path      = local.paths.data_path
   }
   env = [
     "REDIS_HOST=${docker_container.redis.name}",
     "MASTER_IP=${docker_container.master.name}:${docker_container.master.ports[0].internal}",
-    "WORKER_NAME=worker-2:5000"
+    "WORKER_NAME=worker-2:${local.ports.worker_internal_port}"
   ]
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
 
 resource "docker_image" "frontend" {
   name         = "${local.docker_registry}/frontend:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.frontend"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.frontend"
   }
 }
 
@@ -155,21 +171,21 @@ resource "docker_container" "frontend" {
   image = docker_image.frontend.image_id
   name  = "frontend"
   ports {
-    internal = 80
-    external = 7000
+    internal = local.ports.frontend_internal_port
+    external = local.ports.frontend_external_port
   }
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
 
 resource "docker_image" "loadbalancer" {
   name         = "${local.docker_registry}/loadbalancer:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.loadbalancer"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.loadbalancer"
   }
 }
 
@@ -177,21 +193,21 @@ resource "docker_container" "loadbalancer" {
   image = docker_image.loadbalancer.image_id
   name  = "loadbalancer"
   ports {
-    internal = 80
-    external = 5000
+    internal = local.ports.loadbalancer_internal_port
+    external = local.ports.loadbalancer_external_port
   }
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
 
 resource "docker_image" "entrypoint" {
   name         = "${local.docker_registry}/entrypoint:latest"
   keep_locally = true
   build {
-    path       = local.context_path
-    dockerfile = "${local.dockerfiles_path}/Dockerfile.entrypoint"
+    path       = local.paths.context_path
+    dockerfile = "${local.paths.dockerfiles_path}/Dockerfile.entrypoint"
   }
 }
 
@@ -199,11 +215,11 @@ resource "docker_container" "entrypoint" {
   image = docker_image.entrypoint.image_id
   name  = "entrypoint"
   ports {
-    internal = 80
-    external = 8000
+    internal = local.ports.entrypoint_internal_port
+    external = local.ports.entrypoint_external_port
   }
   networks_advanced {
     name = docker_network.network.name
   }
-  restart = "unless-stopped"
+  restart = local.container_restart_policy
 }
